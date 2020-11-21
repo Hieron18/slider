c3b14cb38991958436da6a5a314d7b1859ff993e
Hieron18 <68967210+Hieron18@users.noreply.github.com>
2020-11-21 18:23:27 +0300

    add

---
+++ slider/js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
